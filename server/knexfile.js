020ae638f88ede31ee7d0af7a31e37049b75bf20
rhysgoehring <rhysgoehring@gmail.com>
2019-07-19 14:42:51 -0600

    changed names of env vars related to pg production connection

--- server/knexfile.js
+++ server/knexfile.js
@@ -7,11 +7,11 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      host: process.env.RDS_HOSTNAME,
-      port: process.env.RDS_PORT,
-      user: process.env.RDS_USERNAME,
-      password: process.env.RDS_PASSWORD,
-      database: process.env.RDS_DB_NAME,
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
     },
     pool: {
       min: 2,
