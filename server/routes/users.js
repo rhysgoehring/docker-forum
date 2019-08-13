6caa1da45fd1c9cf3d72e237f7f1236ee81e4b04
rhysgoehring <rhysgoehring@gmail.com>
2019-08-13 14:32:06 -0600

    added sign in route to server/api

--- server/routes/users.js
+++ server/routes/users.js
@@ -39,10 +39,41 @@ router.post("/signup", async (req, res, next) => {
     token = await jwt.sign({ user }, process.env.JWT_KEY);
   } catch (err) {
     console.log("/users/signup error", err);
-    next();
+    next(err);
   }
-  console.log("token", token);
-  console.log("user", user);
+
+  return res.json({ token, user });
+});
+
+router.post("/signin", async (req, res, next) => {
+  let token = "";
+  let user = {};
+  try {
+    const { username, password } = req.body;
+
+    // Look for user account:
+    const userQuery = await knex("users")
+      .returning("*")
+      .where("username", username);
+    if (!userQuery.length) {
+      return res.json({ error: "Account not found, please sign up." });
+    }
+    [user] = userQuery;
+
+    // Compare passwords:
+    const validPassword = await bcrypt.compare(password, user.password);
+    delete user.password;
+
+    if (!validPassword) {
+      return res.json({ error: "Invalid Password" });
+    }
+
+    // Create token:
+    token = await jwt.sign({ user }, process.env.JWT_KEY);
+  } catch (err) {
+    console.log("/users/signin err", err);
+    next(err);
+  }
   return res.json({ token, user });
 });
 
